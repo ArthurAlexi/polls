1eae95051a1380009a2943e4e6f733894e56cf06
Arthur Alexi <arthuralexi@hotmail.com>
2024-03-01 07:44:49 -0300

    feat: add the publish message when a poll receive a vote

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -3,6 +3,7 @@ import { randomUUID } from "node:crypto";
 import { prisma } from "../../lib/prisma";
 import z from "zod";
 import { redis } from "../../lib/redis";
+import { voting } from "../../utils/voting-pub-sub";
 
 
 export async function voteOnPoll(app: FastifyInstance) {
@@ -39,7 +40,12 @@ export async function voteOnPoll(app: FastifyInstance) {
                     }
                 })
 
-                await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId)
+                const votes = await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId)
+                voting.publish(pollId, {
+                    pollOptionId: userPreviousVoteOnPoll.pollOptionId,
+                    votes: Number(votes)
+                })
+
             } else {
                 return reply.status(400).send({ 'message': 'You already voted on this poll.' })
             }
@@ -64,7 +70,12 @@ export async function voteOnPoll(app: FastifyInstance) {
             }
         })
 
-        await redis.zincrby(pollId, 1, pollOptionId)
+        const votes = await redis.zincrby(pollId, 1, pollOptionId)
+        
+        voting.publish(pollId, {
+            pollOptionId,
+            votes: Number(votes)
+        })
 
         return reply.status(201).send()
     })
