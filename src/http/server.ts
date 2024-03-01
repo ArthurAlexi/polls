66b5ade24de57c584417b4a27384ffec49b4b27f
Arthur Alexi <arthuralexi@hotmail.com>
2024-03-01 07:45:49 -0300

    feat: add Websockt to track voting in a poll

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,7 +2,9 @@ import fastify from "fastify";
 import { createPoll } from "./routes/create-poll";
 import { getPoll } from "./routes/get-poll";
 import { voteOnPoll } from "./routes/vote-on-poll";
-import cookie from "@fastify/cookie"
+import cookie from "@fastify/cookie";
+import websocket from "@fastify/websocket";
+import { pollResults } from "./ws/poll-results";
 
 
 const app = fastify()
@@ -20,6 +22,8 @@ app.register(cookie, {
 app.register(createPoll)
 app.register(getPoll)
 app.register(voteOnPoll)
+app.register(websocket)
+app.register(pollResults)
 
 app.listen({
     port: 3333
